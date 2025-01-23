11f0d0bf21f5a69b3c9e402bb5e6fb3d42085ccb
Rishirk2107 <rishikumarss2004@gmail.com>
2025-01-23 21:16:41 +0530

    !fix: main.py

--- server/main.py
+++ server/main.py
@@ -19,6 +19,6 @@ app.include_router(api_router)
 async def root():
     return {"message": "Welcome to the FastAPI MongoDB App!"}
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
